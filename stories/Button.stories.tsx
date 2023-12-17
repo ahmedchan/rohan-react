cebd0efd7a88325a45dc39002f32ba087f1d7772
“ahmedchan” <mr.ahmedsaeed1@gmail.com>
2023-12-17 13:54:20 +0200

    fix: fix conflict

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Meta, StoryObj } from '@storybook/react';
-import Button, { ButtonProps } from '../src/Button/Button';
+import React from "react"
+import { Meta, StoryObj } from "@storybook/react"
+import Button, { ButtonProps } from "../src/Button/Button"
 
 const meta: Meta<typeof Button> = {
-  title: 'Button',
-  component: Button,
-  argTypes: {
-    children: {
-      control: {
-        type: 'Button',
-      },
-    },
-  },
-  parameters: {
-    controls: { expanded: true },
-  },
-};
-export default meta;
-type Story = StoryObj<typeof meta>;
+   title: "Button",
+   component: Button,
+   argTypes: {
+      children: {
+         control: {
+            type: "Button"
+         }
+      }
+   },
+   parameters: {
+      controls: { expanded: true }
+   }
+}
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Base: Story = {
-  args: {
-    children: 'Welcome',
-  },
-};
+   args: {
+      children: "Welcome",
+      variant: "primary",
+      modifier: "outline",
+      rounded: true
+   }
+}
